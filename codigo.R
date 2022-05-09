ce97271ec4d2c084e5a6da2a9a7eb8f66be0659f
Eduardo Gutierrez <egutierrez@securesoftcorp.com>
2022-05-08 21:20:58 -0500

    bebecita!!

--- codigo.R
+++ codigo.R
@@ -16,11 +16,25 @@ latam_smbv1_by_country <- read_excel("SMBv1_LATAM.xlsx", sheet = "location.count
 latam_smbv1_by_os.vendor <- read_excel("SMBv1_LATAM.xlsx", sheet = "operating_system.vendor")
 head("latam_smb_by_os")
 
+# GRAFICO SMB PERU POR ISP
 df.company <- as.data.frame(pe_smb_by_company, header = FALSE)
 df.company.NA <- tidyr::drop_na(df.company)
-n <- 1;N <- 
-par(mfrow = c(1, 1), mar = c(18, 3, 1, 1))
+n <- 1;N <- 10
+par(mfrow = c(1, 1), mar = c(18, 4, 1, 1))
 barplot(height = df.company.NA$hosts[n:N],names = df.company.NA$autonomous_system.name[n:N],las = 2, col = "black", main = "SMB by Company")
+
+# GRAFICO SMB LATAM POR PAIS
+df.latam.country <- as.data.frame(latam_smb_by_country, header = FALSE)
+df.latam.country.NA <- tidyr::drop_na(df.latam.country)
+n <- 1;N <- 10
+par(mfrow = c(1, 1), mar = c(10, 4, 1, 1))
+barplot(height = df.latam.country.NA$hosts,names = df.latam.country.NA$location.country,las = 2, col = "black", main = "SMB by Company")
+
+
+
+
+
+
 
 ## NO USAR ##
 #dataframe_order <- spread(df.company,"autonomous_system.name","hosts")
